e3f1979ed828467a4709191beeec4f1bb5cf61e1
DP <DP@localhost>
2016-02-25 11:53:08 +0800

    Initial Codes: 1. Passing of Variable - OK 2. Login page - Incomplete (no design) 3. Signup part - Incomplete 4. Userpage - Nothing yet

---
+++ userpage.php
@@ -0,0 +1,72 @@
+<html lang="en">
+<head>
+  <!-- Theme Made By www.w3schools.com - No Copyright -->
+  <title>LifeTrackr</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <link href="http://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <link href="login.css" rel="stylesheet">
+  <link href="http://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="50">
+
+<?php
+/*  
+	This is the page for receiving the posted values.
+	Use checking for posted values to refrain errors. :)
+*/ 	
+
+	if (!empty($_POST)) {
+		
+		$logged_num = $_POST["eadd_log"];
+		$signed_num = $_POST["pwd_log"];
+	}
+	
+	$log="11277920";
+	$pass="2hutru8e";
+	
+?>
+
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+				<div class="navbar-header active">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+					</button>
+					<a class="navbar-brand" href="#home">LifeTrackr</a>
+				</div>
+				<div class="collapse navbar-collapse" id="myNavbar">
+				  	<ul class="nav navbar-nav navbar-right ">
+						<li>
+							
+						</li>
+				  	</ul>
+				</div>
+		</div>
+	</nav>
+
+		
+	<div class="container signup">
+		<div class="row">
+			<div class="col-xs-6">
+				<?php 
+						$out_u = strcmp($logged_num,$log);
+						$out_p = strcmp($signed_num,$pass);
+						
+						if($out_u==0 && $out_p==0){
+							echo $logged_num;
+						}else{
+							echo "MALI";
+						}
+				?>
+			</div>
+			<div class="col-xs-6">
+
+			</div>
+		</div>
+	</div>
+</body>
+</html>
