cb120be7c7f51bcb56225fd1fd4fe97f7b48e017
starflame765 <starflame765@localhost>
2016-03-21 17:22:11 +0800

    Functions Added: Temporary Marker auto Hide

---
+++ login.php
@@ -0,0 +1,106 @@
+<html lang="en">
+<head>
+  <!-- Theme Made By www.w3schools.com - No Copyright -->
+  <title>LifeTrackr</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1 ,maximum-scale=1,user-scalable=no">
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="HandheldFriendly" content="true">
+  
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <link href="http://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <link href="login.css" rel="stylesheet" media="screen and (min-width:0)">
+  <link href="http://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+  
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="50">
+
+	<nav class="navbar navbar-default navbar-fixed-top" role = "navigation">
+		<div class="container bar-align">
+				<div class="navbar-header active" id ="logo">
+					  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=#myNavbar>
+
+					  	Sign up!
+
+					  </button>
+					<a class="navbar-brand" href="#home"><!-- LifeTracker --></a>
+				</div>
+				<div class="collapse navbar-collapse" id="myNavbar">
+					<form action="checklogin.php" method="post">
+					  	<ul class="nav navbar-nav navbar-right ">
+							<li>
+								<button class="btn btn-default" type="submit" value="submit" id="login-but">Sign Up! </button>
+							</li>
+
+
+					  	</ul>
+					</form>
+				</div>
+		</div>
+	</nav>
+	
+	<div class = "grid">
+		<div class = "row">
+				
+
+		      <div class = "col-xs-1">
+						
+				</div>
+				
+		        <div class = "col-xs-4 col-md-6">
+		        	 <form action="gosignup.php" method="post">
+
+		                <div class"col-xs-12 col-md-12">
+		                    <div class ="col"><center><h3>WELCOME!</h3></center>
+		                      <div class="row">
+
+		                        <div class="col-xs-12 col-md-12 box">
+												<div class="form-group">
+													<input type="text" class="form-control" id="eadd-log" placeholder="Username" name="uname_log">
+												</div>
+												<div class="form-group">
+													<input type="password" class="form-control" id="pwd-log" placeholder="Password" name="pwd_log">
+												</div>
+												<button class="btn btn-default" type="submit" value="submit" id="sign-but">Log In</button>
+									</div>
+
+		                      </div>
+		                    </div>
+
+
+		                 </div>
+		          	
+         			 </form>      
+		        	
+					
+		        </div>
+
+		       
+
+
+				<div class ="col-xs-6 col-md-6 col-wd-1">
+		          <div class ="col">6column</div>
+		        </div>
+		     
+		      <div class = "col-xs-1">
+						
+				</div>
+
+		</div>
+	
+
+
+	</div>
+
+
+	    
+
+
+</body>
+</html>
---
+++ userpage.php
@@ -0,0 +1,122 @@
+<html lang="en">
+<head>
+  <title>LifeTrackr</title>
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <link href="http://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <link href="userpage.css" rel="stylesheet">
+  <link href="sidebar.css" rel="stylesheet">
+
+  <script src='//ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js'></script>
+  <!-- <script src='dynamicpage.js'></script> -->
+  <!-- <script src='switchpage.js'></script> -->
+
+  <!-- <link href="css/simple-sidebar.css" rel="stylesheet"> -->
+
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA-QcrS-bymcrFPClDmuA4A3RMVZsvQCuQ&signed_in=false"></script>
+  <script src="googlemaps.js"></script>
+</head>
+<body>
+
+<?php
+
+session_start();
+  $con = mysqli_connect("ap-cdbr-azure-southeast-b.cloudapp.net","bdd92f8752ef7e","fdb4d70b","lifetrackr");
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  } else {
+      $query = "select firstName,lastName from userinfo where userName = '".$_SESSION["myuser"]."'";
+      $result = mysqli_query($con, $query) or mysqli_error($con);
+      while ($row = mysqli_fetch_array($result)) {
+          $_SESSION["fname"] = $row[0];
+        $_SESSION["lname"] = $row[1];
+      }
+      
+  }
+?>
+
+  <nav class="navbar">
+    <div class="container-fluid">
+      <div class="navbar-center navbar-header">
+        <a  class="navbar-brand" >LF</a>
+      </div>
+        <ul class="nav navbar-nav">
+      <li>
+        <a href="#menu-toggle" id="menu-toggle"><span class="glyphicon glyphicon-menu-hamburger"></span>  </a>
+      </li>
+      <li>
+        <a href="userpage.php" id="home-but"><span class="glyphicon glyphicon-home"></span> Home</a>
+      </li>
+      <li>
+        <a href="moments.php" id="moments-but"><span class="glyphicon glyphicon-film"></span> Moments</a>
+      </li>
+      <li>
+        <a href="notifications.php" id="notif-but" class="popper" data-toggle="popover" data-trigger="focus"><span class="glyphicon glyphicon-bell"></span> Notifications</a>
+      </li>
+        </ul>
+    </div>
+  </nav>
+  <div id="wrapper" class="toggled">
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav" >
+                <li class="sidebar-brand" >
+                    <a href="profiler.php">
+                        <img src="img.jpg" class="imgicon"><br>
+                        <p>
+                        <?php
+                          echo $_SESSION["fname"]." ". $_SESSION["lname"];
+                        ?></p>
+                    </a>
+                </li>
+                <!-- <hr width="70%"> -->
+                <div class="sidebar-coms">
+                <li>
+                    <a href="#">My Moments</a>
+                </li>
+                <li>
+                    <a href="#">Favorites</a>
+                </li>
+                <li>
+                    <a href="#">Friends</a>
+                </li>
+                <li>
+                    <a href="#">Search</a>
+                </li>
+                <li>
+                    <a href="#">Settings</a>
+                </li>
+                <li>
+                    <a href="#">Logout</a>
+                </li>
+                </div>
+            </ul>
+        </div>
+    </div>
+
+
+    <section id="main-content">
+      <!-- <h2>Home</h2> -->
+      <div id="map"></div>
+    </iframe>
+    </section>
+
+    
+
+    <!-- <script src="js/jquery.js"></script>
+
+    Bootstrap Core JavaScript
+    <script src="js/bootstrap.min.js"></script> -->
+
+    <!-- Menu Toggle Script -->
+    <script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    </script>
+
+</body>
+</html>
