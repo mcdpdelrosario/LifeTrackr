1cee8bcc5db4cc2958292afc78caa6c1bafb3e99
Angel <Angel@localhost>
2016-03-29 09:57:58 -0700

    Commit moments.php and notifications

--- notifications.php
+++ notifications.php
@@ -1,27 +1,22 @@
-<!-- The new loginpage -->
 
 <html lang="en">
 <head>
-
   <title>LifeTrackr</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1 ,maximum-scale=1,user-scalable=no">
-
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <meta name="HandheldFriendly" content="true">
-  
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-  <link href="http://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
-  <link href="notifications.css" rel="stylesheet" media="screen and (min-width:0)">
   <link href="http://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
-<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
-<link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
-
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <link href="userpage.css" rel="stylesheet">
+  <link href="sidebar.css" rel="stylesheet">
 
+  <script src='//ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js'></script>
+  <!-- <script src='js/jquery.ba-hashchange.min.js'></script> -->
+  <!-- <script src='dynamicpage.js'></script> -->
+  <!-- <script src='switchpage.js'></script> -->
 
-  
+  <!-- <link href="css/simple-sidebar.css" rel="stylesheet"> -->
+
+  <!--  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA-QcrS-bymcrFPClDmuA4A3RMVZsvQCuQ&signed_in=true"></script>
+  <script src="js/googlemaps.js"></script> -->
 </head>
 <body>
 
@@ -45,154 +40,116 @@ if (mysqli_connect_errno())
   }
 ?>
 
-
- <nav class="navbar navbar-default navbar-fixed-top">
-
-    
-    <div class="container bar-align">
-
-    
-        <div class="navbar-header active">
-             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar" id = "hamburgerbutton"><span class="glyphicon glyphicon-menu-hamburger"></span> 
-            </button>
-
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbars" id = "searchbutton"><span class="glyphicon glyphicon-search"></span> 
-            </button>
-
-             <button type="button" class="navbar-toggle" data-toggle="collapse" id = "listbutton"><span class="glyphicon glyphicon-option-vertical"></span> 
-            </button>
-
-        </div>
-
-        <div class = "logo">
-            LF
-            </div>
-
-         <div class="collapse navbar-collapse" id="myNavbar">
-            <ul class="nav navbar-nav">
-<!--                 <li>
-                  <a href="#menu-but" id="menu-but"><span class="glyphicon glyphicon-user"></span> Profile</a>
-                </li> -->
+  <nav class="navbar">
+    <div class="container-fluid">
+      <div class="navbar-center navbar-header">
+        <a  class="navbar-brand" >LF</a>
+      </div>
+        <ul class="nav navbar-nav">
+      <li>
+        <a href="#menu-toggle" id="menu-toggle"><span class="glyphicon glyphicon-menu-hamburger"></span>  </a>
+      </li>
+      <li>
+        <a href="userpage.php" id="home-but"><span class="glyphicon glyphicon-home"></span> Home</a>
+      </li>
+      <li>
+        <a href="moments.php" id="moments-but"><span class="glyphicon glyphicon-film"></span> Moments</a>
+      </li>
+      <li>
+        <a href="notifications.php" id="notif-but" class="popper" data-toggle="popover" data-trigger="focus"><span class="glyphicon glyphicon-bell"></span> Notifications</a>
+      </li>
+        </ul>
+    </div>
+  </nav>
+  <div id="wrapper" class="toggled">
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav" >
+                <li class="sidebar-brand" >
+                    <a href="profiler.php">
+                        <img src="img.jpg" class="imgicon"><br>
+                        <p>
+                        <?php
+                          echo $_SESSION["fname"]." ". $_SESSION["lname"];
+                        ?></p>
+                    </a>
+                </li>
+                <!-- <hr width="70%"> -->
+                <div class="sidebar-coms">
                 <li>
-                  <a href="userpage.php" id="signup-but"><span class="glyphicon glyphicon-home"></span> Home</a>
+                    <a href="#">Favorites</a>
                 </li>
                 <li>
-                  <a href="moments.php" id="signup-but"><span class="glyphicon glyphicon-film"></span> Moments</a>
+                    <a href="#">Friends</a>
                 </li>
                 <li>
-                  <a href="notifications.php" id="signup-but" class="popper" data-toggle="popover" data-trigger="focus"><span class="glyphicon glyphicon-bell"></span> Notifications</a>
+                    <a href="#">Search</a>
                 </li>
-
-                
-                  
+                <li>
+                    <a href="#">Settings</a>
+                </li>
+                <li>
+                    <a href="#">Logout</a>
+                </li>
+                </div>
+            </ul>
         </div>
     </div>
-        <div class="collapse navbar-collapse" id="myNavbars">
-           <ul class="nav navbar-nav">
-             <li>
-                      <form>
-                      <input type="search" class="form-control" placeholder=" Search" />
-                     <i class="form-control-feedback glyphicon glyphicon-search" id = "search-but"></i></form>   
-                </li>
-            </ul>
-            </li>
-           </ul>
-         </div> 
 
-        <div class="collapse navbar-collapse" id="mySettings">
-           <ul class="nav navbar-nav">
-            
-                 <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-option-vertical" ></span></a>
-                    <ul class="dropdown-menu">
-                      <?php
-                        include "list.html";
-                      ?>
-            </ul>
-            </li>
-           </ul>
-         </div> 
-
-
-      
-
-
-  </nav>
   
 
-    <div id="wrapper">
-        <div id="sidebar-wrapper">
- 
-            <ul class="sidebar-nav">
-               <?php
+    <section id="main-content">
+      <div class="panel-group" >
+        <div class="panel panel-default">
+          <!-- <form action="goprofiler.php" method="post"> -->
+            <div class="panel-heading plogh">
+              <center><h4>Notifications</h4></center>
+            </div>
+            <div class="panel-body plogb">
+            <div class="col-xs-2"></div>
+                <div class="col-xs-8">
 
-                  include "list.html";
+                  <?php
+                    $con = mysqli_connect("ap-cdbr-azure-southeast-b.cloudapp.net","bdd92f8752ef7e","fdb4d70b","lifetrackr");
 
-               ?>
-            </ul>
-           
-        </div>
-        <!-- /#sidebar-wrapper -->
-
-
-
-    </div>
-
-
-   <div class="panel-group" >
-          <div class="panel panel-default">
-            <!-- <form action="goprofiler.php" method="post"> -->
-              <div class="panel-heading plogh">
-                <center><h4>Notifications</h4></center>
-              </div>
-
-              <div id = "main" class = "container">
-
-              <div class="panel-body plogb">
-              <div class="col-xs-2"></div>
-                  <div id = "box" class="col-xs-8">
-
-                    <?php
-                      $con = mysqli_connect("ap-cdbr-azure-southeast-b.cloudapp.net","bdd92f8752ef7e","fdb4d70b","lifetrackr");
-
-                    // Check connection
-                    if (mysqli_connect_errno())
-                      {
-                      echo "Failed to connect to MySQL: " . mysqli_connect_error();
-                      } else {
-                          $query = "SELECT * FROM userinfo AS ui INNER JOIN notifications AS noti ON ui.userName = noti.from_username WHERE noti.username = '".$_SESSION["myuser"]."' ORDER BY time_taken DESC";
-                          $result = mysqli_query($con, $query) or mysqli_error($con);
-                          while ($row = mysqli_fetch_array($result)) {
-                             $_SESSION["uname_notif"] = $row['username'];
-                             $_SESSION["fname_sender_notif"] = $row['firstName'];
-                             $_SESSION["lname_sender_notif"] = $row['lastName'];
-                             $_SESSION["uname_sender_notif"] = $row['from_username'];
-                             $_SESSION["message_notif"] = $row['notif_message'];
-                             $_SESSION["time_notif"] = $row['time_taken'];
-                             ?>
-                             <div class="inner-content">
-                               <div class="panel panel-default">
-                                  <div class="panel-heading">
-                                    <p><b><?=$_SESSION["fname_sender_notif"]?> <?=$_SESSION["lname_sender_notif"]?></b> @<?=$_SESSION["uname_sender_notif"]?> <?=$_SESSION["time_notif"]?></p>
-                                  </div>
-                                  <div class="panel-body">
-                                    <p><?=$_SESSION["message_notif"]?></p>
-                                  </div>
-                               </div>
+                  // Check connection
+                  if (mysqli_connect_errno())
+                    {
+                    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+                    } else {
+                        $query = "SELECT * FROM userinfo AS ui INNER JOIN notifications AS noti ON ui.userName = noti.from_username WHERE noti.username = '".$_SESSION["myuser"]."' ORDER BY time_taken DESC";
+                        $result = mysqli_query($con, $query) or mysqli_error($con);
+                        while ($row = mysqli_fetch_array($result)) {
+                           $_SESSION["uname_notif"] = $row['username'];
+                           $_SESSION["fname_sender_notif"] = $row['firstName'];
+                           $_SESSION["lname_sender_notif"] = $row['lastName'];
+                           $_SESSION["uname_sender_notif"] = $row['from_username'];
+                           $_SESSION["message_notif"] = $row['notif_message'];
+                           $_SESSION["time_notif"] = $row['time_taken'];
+                           ?>
+                           <div class="inner-content">
+                             <div class="panel panel-default">
+                                <div class="panel-heading">
+                                  <p><b><?=$_SESSION["fname_sender_notif"]?> <?=$_SESSION["lname_sender_notif"]?></b> @<?=$_SESSION["sender_notif"]?> <?=$_SESSION["time_notif"]?></p>
+                                </div>
+                                <div class="panel-body">
+                                  <p><?=$_SESSION["message_notif"]?></p>
+                                </div>
                              </div>
-                          <?php
-                          }
+                           </div>
+                        <?php
                         }
-                    ?>
-                    <br>
-                    <!-- <a href="#" id="load-more">Load More</a> -->
-                    <button class="btn btn-default" id="load-more"><span class="glyphicon glyphicon-repeat"></span><br>Load</button>
-                    </div>
+                      }
+                  ?>
+                  <br>
+                  <!-- <a href="#" id="load-more">Load More</a> -->
+                  <button class="btn btn-default" id="load-more"><span class="glyphicon glyphicon-repeat"></span><br>Load</button>
                   </div>
-                 </div> 
-              </div>
-          </div>
-         
-<!--     </section> -->
+                </div>
+            </div>
+        </div>
+    </section>
+      
+    </section>
     <script>
       $(function(){
           $(".inner-content").slice(0, 3).show(); // select the first ten
@@ -217,12 +174,6 @@ if (mysqli_connect_errno())
         $("#wrapper").toggleClass("toggled");
     });
     </script>
-  
- 
-
-
-      
-
 
 </body>
 </html>
