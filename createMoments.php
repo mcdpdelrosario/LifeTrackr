f7954cca9a2f591a8542f1e8e486322fc0b0bb18
DP <DP@localhost>
2016-03-29 23:37:55 +0800

    Update: 1. Slight modifications to createMoments New File: 1. pinMoments

--- createMoments.php
+++ createMoments.php
@@ -1,14 +1,16 @@
 <?php
-	session_start();
-	$con = mysqli_connect("ap-cdbr-azure-southeast-b.cloudapp.net","bdd92f8752ef7e","fdb4d70b","lifetrackr");	
-	
+	// session_start();
+	$con = mysqli_connect("ap-cdbr-azure-southeast-b.cloudapp.net","bdd92f8752ef7e","fdb4d70b","lifetrackr");
+
 	$longitude=$_GET["moment_Lng"];
 	$latitude=$_GET["moment_Lat"];
-	
+	$message=$_GET["moment_Message"];
+	$img=$_GET["moment_Img"];
 	date_default_timezone_set('Asia/Taipei');
-	
-	$date=date("Y-m-d h:i:sa");
-	$query = "INSERT INTO moments (`long`, `lat`,`time_stamp`) VALUES(".$longitude.",".$latitude.",'".$date."')";
-	$result = mysqli_query($link,$query);
+	$date=date("Y-m-d h:i:s");
+
+	$query = "INSERT INTO moments (`longitude`, `latitude`,`timestamp`) VALUES(".$longitude.",".$latitude.",'".$date."')";
+	$result = mysqli_query($con,$query);
+
 	echo $query;
 ?>
