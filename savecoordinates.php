bd1d5c1847dde3f3107ec32395cb8da5cc5147bb
Angel <Angel@localhost>
2016-03-30 00:51:47 -0700

    Navbar.php has been included to all pages Search bar = done Default template

---
+++ savecoordinates.php
@@ -0,0 +1,22 @@
+<?php
+	
+	// $userID = $_GET["userID"];
+session_start();
+	$longitude = $_GET["longitude"];
+	$latitude = $_GET["latitude"];
+	$moments = $_GET["moments-text"];
+	// $image_user = $_POST["moments-img"];
+	date_default_timezone_set('Asia/Taipei');
+	$date=date("Y-m-d h:i:s");
+
+	$link = mysqli_connect("ap-cdbr-azure-southeast-b.cloudapp.net","bdd92f8752ef7e","fdb4d70b","lifetrackr");
+
+	$query = "INSERT INTO moments (`username`,`moments_message`,`long`, `lat`,`timestamp`) VALUES('".$_SESSION["myuser"]."','".$moments."',".$longitude.",".$latitude.",'".$date."')";
+				
+	$result = mysqli_query($link,$query);
+	
+	echo $query;
+	echo "<br/>";
+
+	echo mysqli_error($link);
+?>
