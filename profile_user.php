a7c49006241e3f74966c214235350e86e5a9c19a
Angel <Angel@localhost>
2016-03-30 17:00:14 -0700

    Moved the .col to userpage.css from navbar.css Working on the profile_user.php

---
+++ profile_user.php
@@ -0,0 +1,61 @@
+
+<html lang="en">
+<head>
+  <title>LifeTrackr</title>
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <link href="http://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <link href="navbar.css" rel="stylesheet">
+  <link href="userpage.css" rel="stylesheet">
+
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src='//ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js'></script>
+</head>
+<body>
+<?php
+  session_start();
+?>
+
+	<!-- palitan mo nalang ng navbar mo-->
+  
+                    <?php
+                        include "navbar.php";
+                      ?>
+
+
+<div class="grid">
+  <div class="row">
+   <div class ="col-xs-5">
+      <div class="col">
+          <img id="profile_pic" src="img.jpg">
+              <a href="profile_user.php"><h4 id="myname"><?=$_SESSION["fname"]?> <?=$_SESSION["lname"]?> <br> </h4></a>
+              <p id="username">@<?=$_SESSION["myuser"]?></p>
+              <a href="profiler.php"><button class="btn btn-default edit-profile">Edit Profile</button></a> 
+      </div>
+    </div>      
+  </div>
+</div>
+
+
+
+
+  <script>
+    $(function(){
+        $(".inner-content").slice(0, 3).show(); // select the first ten
+        $("#load-more").click(function(e){ // click event for load more
+            e.preventDefault();
+            $(".inner-content:hidden").slice(0, 3).show(); // select next 10 hidden divs and show them
+            if($(".inner-content:hidden").length == 0){ // check if any hidden divs still exist
+                alert("No more Moments!"); // alert if there are none left
+            }
+        });
+    });
+  </script>
+
+      <script src="js/jquery-2.2.1.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
