8863e0cc6aca65e2a457e8c85579e92f7f1e4d9d
Angel <Angel@localhost>
2016-03-30 20:01:12 -0700

    Made the profile_user.php container1 responsive CHanged the alignment of logo on middle devices

--- profile_user.php
+++ profile_user.php
@@ -23,21 +23,30 @@
                       ?>
 
 
-<div class="grid">
-  <div class="row">
-   <div class ="col-xs-5">
-      <div class="col">
-          <img id="profile_pic" src="img.jpg">
+
+  
+  <div class = "grid">
+    <div class = "row">
+      <div class="col-md-4 col-wd-12" id="container1-profileuser">
+        <div class="panel panel-primary lefty">
+          <div class="panel-heading header">
+          </div>
+          <div class="panel-body">
+            <div id="left-panel">
+              <img id="profile_pic" src="img.jpg">
               <a href="profile_user.php"><h4 id="myname"><?=$_SESSION["fname"]?> <?=$_SESSION["lname"]?> <br> </h4></a>
               <p id="username">@<?=$_SESSION["myuser"]?></p>
-              <a href="profiler.php"><button class="btn btn-default edit-profile">Edit Profile</button></a> 
+              <a href="profiler.php"><button class="btn btn-default edit-profile" id="editprofile-profileuser">Edit Profile</button></a>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>      
+
+      
+    </div>
   </div>
-</div>
 
-
-
+  
 
   <script>
     $(function(){
