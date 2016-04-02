205cb1dc4f8391f69020eaa3c26e2eba68affc6a
Angel <Angel@localhost>
2016-04-02 03:18:39 -0700

    Working on the moments page

---
+++ momentHandler.js
@@ -0,0 +1,16 @@
+var dataMoments;
+
+function createMoment(lat,lng,message){
+	//alert('lat: '+lat+' lng: '+lng+' comments: '+message);
+	var url = "createMoments.php?moment_Lng="+lng+ "&moment_Lat=" + lat+"&moment_Message=" + message;
+	httpGetAsync(url,successQuery);
+}
+function successQuery(){
+	swal("Good job!", "Sent!", "success");
+}
+function pinMoment(){
+	var url = "pinMoments.php";
+	httpGetAsync(url,function(json){
+		dataMoments = JSON.parse(json);
+	});
+}
