34613af4a7700b89b76cded93c633fd752d4641d
starflame765 <starflame765@localhost>
2016-04-02 13:33:21 +0800

    Informative Swal added

--- momentHandler.js
+++ momentHandler.js
@@ -3,10 +3,12 @@ var dataMoments;
 function createMoment(lat,lng,message){
 	//alert('lat: '+lat+' lng: '+lng+' comments: '+message);
 	var url = "createMoments.php?moment_Lng="+lng+ "&moment_Lat=" + lat+"&moment_Message=" + message;
-	httpGetAsync(url,alert);
+	httpGetAsync(url,successQuery);
 }
-function successQuery(){
-	swal("Good job!", "Sent!", "success");
+function successQuery(text){
+	swal("Good job!", text, "success");
+	hideTemporaryMarker();
+
 }
 function pinMoment(){
 	var url = "pinMoments.php";
