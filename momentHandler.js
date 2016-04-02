799976effb219d8fa2764aa12ca2fa095595d733
Angel <Angel@localhost>
2016-04-02 15:26:55 -0700

    Removed some bugs

---
+++ momentHandler.js
@@ -0,0 +1,20 @@
+var dataMoments;
+
+function createMoment(lat,lng,message){
+	//alert('lat: '+lat+' lng: '+lng+' comments: '+message);
+	var url = "createMoments.php?moment_Lng="+lng+ "&moment_Lat=" + lat+"&moment_Message=" + message;
+	httpGetAsync(url,successQuery);
+}
+function successQuery(text){
+	swal("Good job!", text, "success");
+	hideTemporaryMarker();
+
+}
+function pinMoment(){
+	var url = "pinMoments.php";
+	httpGetAsync(url,function(json){
+	 	dataMoments = JSON.parse(json);
+		// console.log(dataMoments);
+	});
+	// httpGetAsync(url,alert);
+}
