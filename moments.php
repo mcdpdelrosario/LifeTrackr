20d79a1f27bcbf6238e9f8e442b9ee61c71e0bf9
Angel <Angel@localhost>
2016-04-03 22:31:11 -0700

    Working on the moments page.

--- moments.php
+++ moments.php
@@ -28,19 +28,64 @@
     <?php
       include "navbar.php";
     ?>
-
 <div class="containera-moments">
-     <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12" id="container1-moments">
+  <div class="row">
+     <div class="col-lg-3 col-md-4 col-sm-4" id="container1-moments">
           <div class="col-lg-4 col-md-4 col-sm-4 col-xs-1">
             <img id="profile-pic-moments" src="img.jpg">
           </div>
+          
+          <div class="col-lg-8 col-md-4 col-sm-4 col-xs-1" id="name">
+            Angel
+          </div>
 
+    </div>
+
+
+   <div class="container2-moments">  
+      <div class="col-lg-9 col-md-9 col-sm-8 col-xs-12">
+        <div class="col-lg-2 col-md-3 col-sm-4 col-xs-3">
+            <img id="container2-profile" src="img.jpg">
+        </div>
+
+        <div class="col-lg-10 col-md-9 col-sm-8 col-xs-6">
+          
+           <input type="text" class="form-control" placeholder=" Make now a moment." id="input-moments"/>
+
+        </div>
+    
+      </div>
+    </div> 
 
   </div>
+
+
 </div>
-                 
 
+<div class="containerb-moments">
 
+<div class="row">
+  <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12" id="container3-moments">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="icons-moments">
+            <ul class="nav nav-pills">
+                <li><a href="#" id="friends-icons" >
+                  <!-- <span class="glyphicon glyphicon-user"id="message-moments"></span> -->Friends
+                  </a></li>
+
+                 <li><a href="#" id="messages-icons" >
+                  <!-- <span class="glyphicon glyphicon-user"id="message-moments"></span> -->Messages
+                  </a></li>
+
+                  <li><a href="#" id="likes-icons">
+                  <!-- <span class="glyphicon glyphicon-user"id="message-moments"></span> -->Likes
+                  </a></li>
+
+          </ul>
+
+       </div>   
+      
+    </div>
+</div>
 
 </body>
 </html>
