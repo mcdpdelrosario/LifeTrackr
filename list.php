8cc2139cbfab18ada887455d9461a6e738459cdb
Nathan <Nathan@localhost>
2016-04-05 21:03:37 +0800

    Query Fix

--- list.php
+++ list.php
@@ -1,50 +1,51 @@
-			   <li>
-                    <?php
-                      $con = mysqli_connect("ap-cdbr-azure-southeast-b.cloudapp.net","bdd92f8752ef7e","fdb4d70b","lifetrackr");
-                      if (mysqli_connect_errno())
-                      {
-                        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-                      } 
-                      else 
-                      {
+<li>
+  <?php
+    $con = mysqli_connect("ap-cdbr-azure-southeast-b.cloudapp.net","bdd92f8752ef7e","fdb4d70b","lifetrackr");
+    
+    if (mysqli_connect_errno())
+    {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    } 
+    else 
+    {
+      $query = "SELECT * FROM userinfo WHERE user_id = '".$_SESSION["myuser"]."'";
+      $result = mysqli_query($con, $query) or mysqli_error($con);
+      
+      while ($row = mysqli_fetch_array($result)) 
+      {
+        $_SESSION["uname"] = $row['user_id'];
+      }
 
-                        $query = "select * from userinfo where username = '".$_SESSION["myuser"]."'";
-                        $result = mysqli_query($con, $query) or mysqli_error($con);
-                        while ($row = mysqli_fetch_array($result)) {
-                          $_SESSION["uname"] = $row['username'];
-                        }
-
-                        //$query = "select first_name,last_name from userinfo where username = '".$_SESSION["myuser"]."'";
-                        $query = "select first_name,last_name from userinfo where username = '".$_SESSION["uname"]."'";
-                        $result = mysqli_query($con, $query) or mysqli_error($con);
+      $query = "SELECT first_name,last_name FROM userinfo WHERE user_id = '".$_SESSION["uname"]."'";
+      $result = mysqli_query($con, $query) or mysqli_error($con);
                         
-                        while ($row = mysqli_fetch_array($result)) 
-                        {
-                          $_SESSION["fname"] = $row['first_name'];
-                          $_SESSION["lname"] = $row['last_name'];
-                        }
-                      }
-                      ?>
-                        <a id="listdropdown-name" href="#">
-                          <?php
-                            echo $_SESSION["fname"]." ". $_SESSION["lname"];
-                          ?>
-                        </a>
-                        
-               </li>	
+      while ($row = mysqli_fetch_array($result)) 
+      {
+        $_SESSION["fname"] = $row['first_name'];
+        $_SESSION["lname"] = $row['last_name'];
+      }
+    }
+  ?>
+  
+  <a id="listdropdown-name" href="#">
+    <?php
+      echo $_SESSION["fname"]." ". $_SESSION["lname"];
+    ?>
+  </a>                        
+</li>	
 
-			   <li>
-                   <a id="listdropdown-buts" href="#"></a>
-               </li>
+<li>
+  <a id="listdropdown-buts" href="#"></a>
+</li>
 
-               <li>
-                  <a href="#" id="listdropdown-buts">My Commits</a>
-               </li>
+<li>
+  <a href="#" id="listdropdown-buts">My Commits</a>
+</li>
 				
-			   <li>
-                  <a href="subpages/settings.php" id="listdropdown-buts">Settings</a>
-               </li>
+<li>
+  <a href="#" id="listdropdown-buts">Settings</a>
+</li>
 
-               <li>
-                  <a href="#" id="listdropdown-buts">Logout</a>
-               </li>
+<li>
+  <a href="#" id="listdropdown-buts">Logout</a>
+</li>
