8b458f382ef844d801ccb2deccdfdba042f2da90
Angel <Angel@localhost>
2016-04-15 13:09:59 -0700

    Done with the notification.php both desktop and phone

--- notifications.php
+++ notifications.php
@@ -1,26 +1,31 @@
 <html lang="en">
-
 <head>
-  
+
   <title>LifeTrackr</title>
   
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1 ,maximum-scale=1,user-scalable=no">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="HandheldFriendly" content="true">
+  
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <link href="http://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <!-- <link href="userpage.css" rel="stylesheet" media="screen and (min-width:0)"> -->
+  <link href="navbar.css" rel="stylesheet" media="screen and (min-width:0)">
+  <link href="userpage.css" rel="stylesheet" media="screen and (min-width:0)">
   <link href="http://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
-  <link href="navbar.css" rel="stylesheet">
-  <link href="userpage.css" rel="stylesheet">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-  <!-- <link href="css/simple-sidebar.css" rel="stylesheet"> -->
+  <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+  <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="text/css" href="dist/sweetalert.css">
 
-  <script src='//ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js'></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-  <!-- <script src='js/jquery.ba-hashchange.min.js'></script> -->
-  <!-- <script src='dynamicpage.js'></script> -->
-  <!-- <script src='switchpage.js'></script> -->
-  <!--  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA-QcrS-bymcrFPClDmuA4A3RMVZsvQCuQ&signed_in=true"></script>
-  <script src="js/googlemaps.js"></script> -->
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="dist/sweetalert.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA-QcrS-bymcrFPClDmuA4A3RMVZsvQCuQ&signed_in=false"></script>
+  <script src="main_Map.js"></script>
+  <script src="momentHandler.js"></script>
 
 </head>
-
 <body>
   <?php
 
@@ -46,8 +51,8 @@
     include "navbar.php";
   ?>
 
-  <section id="main-content">
-    <div class="panel-group" >
+  <section id="main-content" >
+    <div class="panel-group" id="container1-notification">
       <div class="panel panel-default">
         <!-- <form action="goprofiler.php" method="post"> -->
         <div class="panel-heading plogh">
@@ -95,7 +100,7 @@
             ?>
             <br>
             <!-- <a href="#" id="load-more">Load More</a> -->
-            <button class="btn btn-default" id="load-more"><span class="glyphicon glyphicon-repeat"></span><br>Load</button>
+            <button class="btn btn-default" id="notification-load-more"><span class="glyphicon glyphicon-repeat"></span><br>Load</button>
           </div>
         </div>
       </div>
