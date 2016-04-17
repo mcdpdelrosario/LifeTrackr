571c53035e9e187c88482635d949430798f6a222
DP <DP@localhost>
2016-04-17 10:49:36 +0800

    slight modifications on userpage.php

--- userpage.php
+++ userpage.php
@@ -32,23 +32,6 @@
   <?php
     session_start();
     $con = mysqli_connect("ap-cdbr-azure-southeast-b.cloudapp.net","bdd92f8752ef7e","fdb4d70b","lifetrackr");
-  
-    // Check connection
-    if (mysqli_connect_errno())
-    {
-      echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    } 
-    else 
-    {
-      $query = "SELECT first_name,last_name FROM userinfo WHERE user_id = '".$_SESSION["myuser"]."'";
-      $result = mysqli_query($con, $query) or mysqli_error($con);
-      while ($row = mysqli_fetch_array($result)) 
-      {
-        $_SESSION["fname"] = $row['first_name'];
-        $_SESSION["lname"] = $row['last_name'];
-      }
-    }
-    
     include "navbar.php";
   ?>
 
