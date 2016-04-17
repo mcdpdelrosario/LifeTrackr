2e3430c74d9fe22762df2736565d7ee1046dacfe
DP <DP@localhost>
2016-04-17 22:49:45 +0800

    Update: 1. Adding, deleting and accepting between users done 2. ACTIVITY notif done 3. FRIEND REQUESTS notif done 4. ALL notif done

--- deleteFriend.php
+++ deleteFriend.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
-	$user_id_adder=$_GET['friend'];
+	$user_id_fr=$_GET['friend'];
 	date_default_timezone_set('Asia/Taipei');
 	$date=date("Y-m-d h:i:s");
 	$con = mysqli_connect("ap-cdbr-azure-southeast-b.cloudapp.net","bdd92f8752ef7e","fdb4d70b","lifetrackr");
@@ -8,10 +8,10 @@
 	if (mysqli_connect_errno()){
                     echo "Failed to connect to MySQL: " . mysqli_connect_error();
                     } else {
-                          echo $query = "UPDATE friends SET status=1,time_stamp='".$date."' WHERE user_id_fr='".$_SESSION['userid']."' AND user_id_user='".$user_id_adder."'";
-                        		mysqli_query($con, $query) or mysqli_error($con);
-
-                        		$redirect="Location: profile_user.php?user=".$user_id_adder."";
+                          $query = "DELETE FROM friends WHERE user_id_user=".$_SESSION['userid']." AND user_id_fr=".$user_id_fr."";
+                          mysqli_query($con, $query) or mysqli_error($con);	
+                          $redirect="Location: profile_user.php?user=".$user_id_fr."";
                           header($redirect);
 			}
+
 ?>
