3014d45c6d1d7b06e191fe5d494a6153f513aef1
Angel <Angel@localhost>
2016-04-18 00:19:47 -0700

    Fix: Pills DOing notification for now

--- moments.php
+++ moments.php
@@ -144,7 +144,7 @@ session_start();
 <div class="row">
 
   <div class="col-lg-3 col-lg-offset-1 col-md-3 col-md-offset-1 col-sm-4 col-sm-offset-1 col-xs-12" id="container3-moments">
-       <ul class="nav nav-pills">
+       <ul class="nav nav-pills"id="pills-moments">
         <li><a href="#">Friends</a></li>
         <li><a href="#">Likes</a></li>
         <li><a href="#">Messages</a></li>
