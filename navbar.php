3ae8e97db4f86cb12d566b9f2662ae752a75545e
Angel <Angel@localhost>
2016-04-18 09:49:26 -0700

    Commit user glyphicon

--- navbar.php
+++ navbar.php
@@ -59,12 +59,7 @@
         <div class="collapse navbar-collapse" id="mySettings">
            <ul class="nav navbar-nav">
             
-                 <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-option-vertical" ></span></a>
-                    <ul class="dropdown-menu" id="dropdown-settings">
-                      <?php
-                        include "list.php";
-                      ?>
-            </ul>
+                 <li ><a href="activityNotifications.php"><span class="glyphicon glyphicon-user" ></span></a>
             </li>
            </ul>
          </div> 
